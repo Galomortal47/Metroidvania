aab3ba4a0a13d632e1ad57d0bdaa3116471fd1a2
Galomortal47 <44421928+Galomortal47@users.noreply.github.com>
2019-08-17 17:38:55 -0300

    gun engine

--- Scripts/Reload.gd
+++ Scripts/Reload.gd
@@ -1,14 +1,7 @@
 extends Label
 
 func reload():
-	if get_parent().get_node("Bomb").enable:
-		get_parent().get_node("Bomb").reload()
-		get_parent().get_node("Bomb").update_text()
-	
-	if get_parent().get_node("Gun").enable:
-		get_parent().get_node("Gun").reload()
-		get_parent().get_node("Gun").update_text()
-	
-	if get_parent().get_node("Shotgun").enable:
-		get_parent().get_node("Shotgun").reload()
-		get_parent().get_node("Shotgun").update_text()
+	for i in range(0,4):
+		if get_parent().get_child(i).enable:
+			get_parent().get_child(i).reload()
+			get_parent().get_child(i).update_text()

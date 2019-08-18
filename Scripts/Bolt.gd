266107b10f1a00ef27c9f5e00f7bc9e09894b854
Galomortal47 <44421928+Galomortal47@users.noreply.github.com>
2019-08-18 00:32:51 -0300

    bolt

--- Scripts/Bolt.gd
+++ Scripts/Bolt.gd
@@ -1,13 +1,27 @@
 extends Node2D
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+var body2
+var speed = 1
+var accel = 1.1
+var gravity = 10
+var collect = false
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
+func _process(delta):
+	if not $ground_detect.is_colliding():
+		set_position(get_position() + Vector2(0,gravity))
+	if collect:
+		var rotation = get_angle_to(body2.get_position())
+		var dir = Vector2(cos(rotation), sin(rotation))
+		set_position(get_position() + dir * (speed * delta))
+		speed *= accel
+
+func _on_magnetic_body_shape_entered(body_id, body, body_shape, area_shape):
+	if body.is_in_group("player"):
+		collect = true
+		body2 = body
 	pass # Replace with function body.
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _on_destroy_body_shape_entered(body_id, body, body_shape, area_shape):
+	if body.is_in_group("player"):
+		queue_free()
+	pass # Replace with function body.

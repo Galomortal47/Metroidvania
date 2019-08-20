9e258bb90609eb4e702f469d74102da3a1870ad9
Galomortal47 <44421928+Galomortal47@users.noreply.github.com>
2019-08-20 14:51:36 -0300

    working platforms

--- Scripts/Platform Sticker 2.gd
+++ Scripts/Platform Sticker 2.gd
@@ -10,13 +10,16 @@ func stick(var raycast):
 	if raycast.is_colliding() and not has_node("Player"):
 		player = raycast.get_collider()
 		var pos =  player.get_global_position()
+		player.set_global_position(pos - get_position())
 		get_tree().get_root().get_node("Test").remove_child( player)
 		self.add_child( player)
 		player.set_global_position(pos)
 		parent = true
 	elif parent and not raycast.is_colliding() and not get_tree().get_root().get_node("Test").has_node("Player"):
 		var pos = player.get_global_position()
-		self.remove_child(player)
+		if has_node("Player"):
+			self.remove_child(player)
 		player.set_global_position(pos)
-		get_tree().get_root().get_node("Test").add_child(player)
+		if player.get_parent() == null:
+			get_tree().get_root().get_node("Test").add_child(player)
 		var parent = false
